af9e7bbe434ad1e832de478e0bbcce5d47184ca3
CJD115 <connordavis115@gmail.com>
2025-09-10 23:05:57 +0100

    Fixed tabs not opening on the designated tab

--- Assets/_Scripts/MenuController.cs
+++ Assets/_Scripts/MenuController.cs
@@ -1,13 +1,18 @@
-
-// Controls the in-game menu visibility using the Tab key.
-// Attach this script to a GameObject in your scene and assign the menuCanvas in the Inspector.
 using UnityEngine;
 using UnityEngine.InputSystem; // For the new Input System
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     // Reference to the menu UI Canvas GameObject. Set this in the Inspector.
     public GameObject menuCanvas;
+
+    // Reference to the TabController. Set this in the Inspector.
+    public TabController tabController;
+
+    // Reference to all tab buttons. Set these in the Inspector.
+    public Button[] tabButtons;
 
     // Called before the first frame update.
     // Ensures the menu is hidden when the game starts.
@@ -20,10 +25,41 @@ public class MenuController : MonoBehaviour
     // Listens for the Tab key to toggle the menu's visibility using the new Input System.
     void Update()
     {
-        // Toggle menu on/off when Tab is pressed (new Input System)
+        // Toggle menu on/off when Escape is pressed (new Input System)
         if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
         {
-            menuCanvas.SetActive(!menuCanvas.activeSelf);
+            bool willOpen = !menuCanvas.activeSelf;
+            menuCanvas.SetActive(willOpen);
+            if (willOpen && tabController != null)
+            {
+                StartCoroutine(ForceTab0AndRestoreTabs());
+            }
+        }
+    }
+
+    // Coroutine to disable tab buttons, force tab 0, then re-enable buttons
+    private System.Collections.IEnumerator ForceTab0AndRestoreTabs()
+    {
+        // Disable all tab buttons
+        if (tabButtons != null)
+        {
+            foreach (var btn in tabButtons)
+            {
+                btn.interactable = false;
+            }
+        }
+        // Force tab 0
+        tabController.ForceTab0();
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(null);
+        // Wait for end of frame
+        yield return new WaitForEndOfFrame();
+        // Re-enable all tab buttons
+        if (tabButtons != null)
+        {
+            foreach (var btn in tabButtons)
+            {
+                btn.interactable = true;
+            }
         }
     }
 }
--- Assets/_Scripts/TabController.cs
+++ Assets/_Scripts/TabController.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
+
 
 public class TabController : MonoBehaviour
 {
     public Image[] tabImages;
-public GameObject[] pages;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] pages;
+
+    private int currentTab = 0;
+    private bool forceTab = false;
+
     void Start()
     {
         ActivateTab(0);
@@ -13,6 +19,7 @@ public GameObject[] pages;
 
     public void ActivateTab(int tabNo)
     {
+        currentTab = tabNo;
         for (int i = 0; i < pages.Length; i++)
         {
             pages[i].SetActive(false);
@@ -20,6 +27,13 @@ public GameObject[] pages;
         }
         pages[tabNo].SetActive(true);
         tabImages[tabNo].color = Color.white;
+        forceTab = false;
+    }
+
+    // Call this from MenuController to force tab 0
+    public void ForceTab0()
+    {
+        forceTab = true;
+        ActivateTab(0);
     }
 }
- 
